5e4c0a17bb1d3259f0fb54dd00a0887c9f2b39ea
EGRrqq <86194387+EGRrqq@users.noreply.github.com>
2023-02-23 16:35:26 +0300

    fix key prop

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const App = () => {
             <h2>blogs</h2>
             {blogs
                 .map(blog =>
-                    <Blog key={blog.id} blog={blog} />
+                    <Blog key={blog._id} blog={blog} />
                 )}
         </div>
     )
